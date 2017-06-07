f5512011bbdb589bba3a96592a86947e6d5ae64d
Yonhom <953410459@qq.com>
2017-06-07 17:44:51 +0800

    1. set the album detail fragment's toolbar as the action bar of the activity this fragment is in, so the activity can manipulate the toolbar via menu callbacks 2. set the collapsing app bar's properties

--- app/src/main/java/com/xuyonghong/xyhtimber/fragment/AlbumDetailFragment.java
+++ app/src/main/java/com/xuyonghong/xyhtimber/fragment/AlbumDetailFragment.java
@@ -3,22 +3,38 @@ package com.xuyonghong.xyhtimber.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.xuyonghong.xyhtimber.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by xuyonghong on 2017/6/7.
  */
 
 public class AlbumDetailFragment extends Fragment {
+    @BindView(R.id.toolbar)
+    Toolbar toobar;
+
+    @BindView(R.id.app_bar_image)
+    ImageView appBarImage;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_album_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        // init the toolbar
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toobar);
+
         return view;
     }
 }
